63d2a0f616b3151dddcafef6f6f078b6fb7259ea
svensken <vladfolder@gmail.com>
2023-05-15 23:16:59 +0300

    prepare project, linter and ssr

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,29 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from '@angular/core/testing'
+import { AppComponent } from './app.component'
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [AppComponent]
-  }));
+	beforeEach(() =>
+		TestBed.configureTestingModule({
+			imports: [AppComponent],
+		})
+	)
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+	it('should create the app', () => {
+		const fixture = TestBed.createComponent(AppComponent)
+		const app = fixture.componentInstance
+		expect(app).toBeTruthy()
+	})
 
-  it(`should have the 'smnv' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('smnv');
-  });
+	it(`should have the 'smnv' title`, () => {
+		const fixture = TestBed.createComponent(AppComponent)
+		const app = fixture.componentInstance
+		expect(app.title).toEqual('smnv')
+	})
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('smnv app is running!');
-  });
-});
+	it('should render title', () => {
+		const fixture = TestBed.createComponent(AppComponent)
+		fixture.detectChanges()
+		const compiled = fixture.nativeElement as HTMLElement
+		expect(compiled.querySelector('.content span')?.textContent).toContain('smnv app is running!')
+	})
+})
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	standalone: true,
+	imports: [CommonModule, RouterOutlet],
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'smnv';
+	spawn() {
+		console.log('spawn')
+	}
 }
